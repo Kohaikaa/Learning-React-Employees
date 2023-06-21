4be72d8c7573ef4c44aade76e8001cb787a5e34b
Kohaikaa <cherry.designwork@gmail.com>
2023-06-21 16:26:58 +0300

    perfect

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 
 import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import SearchPanel from '../search-panel/search-panel';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
